b577027e7f452e00ccc2b90ed35ec35d0db310f5
An-drew-bit <mackarov.makoroff2016@yandex.ru>
2022-10-14 15:06:10 +0300

    create telegram logger and create new command

--- README.md
+++ README.md
@@ -4,12 +4,17 @@
 ```
 {%project_folder%}: cp ./env.example ./.env
 {%project_folder%}: composer install
-{%project_folder%}: php artisan migrate --seed
-{%project_folder%}: php artisan artisan storage:link
+{%project_folder%}: php artisan shop:install
 ```
 
 ### Для локального запуска
 ```
 {%project_folder%}: php artisan serve
 http://127.0.0.1:8000
+```
+
+### Логи отправляются в телеграм, вам потребуется создать бота и чат с ним
+```
+TELEGRAM_BOT_TOKEN=
+TELEGRAM_CHAT_ID=
 ```
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonInterval;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Http\Kernel;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -30,9 +32,16 @@ class AppServiceProvider extends ServiceProvider
         Model::preventLazyLoading(!app()->isProduction());
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
 
-        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
-            // Notify development team...
-        });
+        DB::whenQueryingForLongerThan(500, fn(Connection $connection) => logger()
+                ->channel('telegram')
+                ->debug('whenQueryingForLongerThan:' . $connection->query()->toSql()));
+
+        $kernel = app(Kernel::class);
+
+        $kernel->whenRequestLifecycleIsLongerThan(
+            CarbonInterval::seconds(4), fn() => logger()
+                ->channel('telegram')
+                ->debug('whenRequestLifecycleIsLongerThan:' . request()->url()));
 
         Password::defaults(fn() => Password::min(8)
             ->letters()
