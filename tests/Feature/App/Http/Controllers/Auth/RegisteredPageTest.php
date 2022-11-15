2ebbbf240abb926fb0dad28db7486ac2e541f2d8
An-drew-bit <mackarov.makoroff2016@yandex.ru>
2022-11-15 13:59:34 +0300

    test actions

--- tests/Feature/App/Http/Controllers/Auth/RegisteredPageTest.php
+++ tests/Feature/App/Http/Controllers/Auth/RegisteredPageTest.php
@@ -20,7 +20,7 @@ class RegisteredPageTest extends TestCase
 
     protected array $request;
 
-    protected function setUp(): void
+    public function setUp(): void
     {
         parent::setUp();
 
