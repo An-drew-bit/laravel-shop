d87b990b0d978f473c83355758fd61d835f3a4d7
An-drew-bit <mackarov.makoroff2016@yandex.ru>
2022-12-07 16:17:55 +0300

    fix error actions

--- app/Http/Controllers/Auth/AuthenticatedController.php
+++ app/Http/Controllers/Auth/AuthenticatedController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
